351c0e2e8c84c4b125c91e1d3edc251cb28321e9
amj358 <amanda.jessup@gmail.com>
2019-05-15 09:31:41 -0400

    Add files via upload

---
+++ Scripts/QuitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    public void Done ()
+    {
+        Debug.Log("he has quit the game");
+
+
+        Application.Quit();
+    }
+}
